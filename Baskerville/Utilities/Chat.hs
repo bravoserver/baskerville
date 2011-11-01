65fd8a7483ee3a4c70411b16798873632b02c1f4
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-11-01 16:22:58 -0700

    Hlint in chat.

--- Baskerville/Utilities/Chat.hs
+++ Baskerville/Utilities/Chat.hs
@@ -4,7 +4,7 @@ import Data.Char
 
 hashStringIndex s = foldr ((+) . ord) 0 s `mod` 15
 
-betaColor i = ['§', ("0123456789abcde" !! i)]
+betaColor i = ['§', "0123456789abcde" !! i]
 consoleColor i =
     "\x1b[" ++ (["1;30", "34", "32", "36", "31", "35", "33", "1;37", "37",
     "1;34", "1;32", "1;36", "1;31", "1;35", "1;33"] !! i) ++ "m"
