592d40749759768f9e1e657e91820c8338f72f23
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-11-01 16:13:14 -0700

    More chat, and tests.

--- Baskerville/Utilities/Chat.hs
+++ Baskerville/Utilities/Chat.hs
@@ -2,4 +2,12 @@ module Baskerville.Utilities.Chat where
 
 import Data.Char
 
-hashStringIndex s = foldr ((+) . ord) 0 s `mod` 16
+hashStringIndex s = foldr ((+) . ord) 0 s `mod` 15
+
+betaColor i = ['§', ("0123456789abcde" !! i)]
+consoleColor i =
+    "\x1b[" ++ (["1;30", "34", "32", "36", "31", "35", "33", "1;37", "37",
+    "1;34", "1;32", "1;36", "1;31", "1;35", "1;33"] !! i) ++ "m"
+
+betaChatName s = betaColor (hashStringIndex s) ++ s ++ "§f"
+consoleChatName s = consoleColor (hashStringIndex s) ++ s ++ "\x1b[0m"
--- Tests.hs
+++ Tests.hs
@@ -16,8 +16,16 @@ unpackNibblesMod2 x = length (unpackNibbles x) `mod` 2 == 0
 packUnpackNibbles x = packNibbles (unpackNibbles x) == x
 
 -- Baskerville.Utilities.Chat
-hashStringIndexRange x = let i = hashStringIndex x in i >= 0 && i < 16
+hashStringIndexRange x = let i = hashStringIndex x in i >= 0 && i < 15
+betaChatNameHead x = head (betaChatName x) == '§'
+betaChatNameSuffix x = isSuffixOf "§f" (betaChatName x)
+consoleChatNamePrefix x = isPrefixOf "\x1b[" (consoleChatName x)
+consoleChatNameSuffix x = isSuffixOf "\x1b[0m" (consoleChatName x)
 
 tests = [("unpackNibbles/mod2", quickCheck unpackNibblesMod2)
         ,("packNibbles.unpackNibbles/id", quickCheck packUnpackNibbles)
-        ,("hashStringIndex/range", quickCheck hashStringIndexRange)]
+        ,("hashStringIndex/range", quickCheck hashStringIndexRange)
+        ,("betaChatName/head", quickCheck betaChatNameHead)
+        ,("betaChatName/suffix", quickCheck betaChatNameSuffix)
+        ,("consoleChatName/prefix", quickCheck consoleChatNamePrefix)
+        ,("consoleChatName/suffix", quickCheck consoleChatNameSuffix)]
