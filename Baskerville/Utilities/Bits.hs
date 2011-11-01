f46ac1bbdf1a353d1da6bcae29944bc710a48556
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-11-01 14:54:26 -0700

    Improve types for bits, adjust tests, add chat to build.

--- Baskerville/Utilities/Bits.hs
+++ Baskerville/Utilities/Bits.hs
@@ -2,9 +2,11 @@ module Baskerville.Utilities.Bits where
 
 import Data.Bits
 
+unpackNibbles :: [Int] -> [Int]
 unpackNibbles (x:xs) = x .&. 0xf : x `shiftR` 4 : unpackNibbles xs
 unpackNibbles [] = []
 
+packNibbles :: [Int] -> [Int]
 packNibbles (x1:x2:xs) = (x1 .|. (x2 `shiftL` 4)) : packNibbles xs
 packNibbles (x:xs) = x:xs
 packNibbles [] = []
--- Tests.hs
+++ Tests.hs
@@ -5,16 +5,19 @@ import Test.QuickCheck
 import Text.Printf
 
 import Baskerville.Utilities.Bits
+import Baskerville.Utilities.Chat
 
 -- From some tutorial somewhere.
 mapper (d, t) = printf "%-30s: " d >> t
 main = mapM_ mapper tests
 
 -- Baskerville.Utilities.Bits
-unpackNibblesMod2 :: [Int] -> Bool
 unpackNibblesMod2 x = length (unpackNibbles x) `mod` 2 == 0
-packUnpackNibbles :: [Int] -> Bool
 packUnpackNibbles x = packNibbles (unpackNibbles x) == x
 
+-- Baskerville.Utilities.Chat
+hashStringIndexRange x = let i = hashStringIndex x in i >= 0 && i < 16
+
 tests = [("unpackNibbles/mod2", quickCheck unpackNibblesMod2)
-        ,("packNibbles.unpackNibbles/id", quickCheck packUnpackNibbles)]
+        ,("packNibbles.unpackNibbles/id", quickCheck packUnpackNibbles)
+        ,("hashStringIndex/range", quickCheck hashStringIndexRange)]
