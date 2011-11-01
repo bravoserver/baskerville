e9db04253d90eda4bf6cf2cc33d42fd518eb028a
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-10-31 22:33:08 -0700

    Slowly start translating code.
    It's gonna be an uphill battle, I can tell.

--- Baskerville/Utilities/Geometry.hs
+++ Baskerville/Utilities/Geometry.hs
@@ -1,1 +1,20 @@
 module Baskerville.Utilities.Geometry where
+
+import Data.Ord (comparing)
+
+data Coord = Coord {cx :: Float, cy :: Float, cz :: Float}
+    deriving (Show, Eq, Ord)
+
+coord2tuple (Coord x y z) = (x, y, z)
+tuple2coord (x, y, z) = Coord x y z
+
+mapCoord f (Coord x y z) = Coord (f x) (f y) (f z)
+
+absCoord = mapCoord abs
+maxCoord (Coord x y z) = maximum [x, y, z]
+
+normalizeCoord c@(Coord x y z) = let m = maxCoord (absCoord c)
+    in Coord (x / m) (y / m) (z / m)
+
+diffCoords (Coord x1 y1 z1) (Coord x2 y2 z2) =
+    Coord (x1 - x2) (y1 - y2) (z1 - z2)
