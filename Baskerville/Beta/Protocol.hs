47f817ce8fef63cd5e567c4afc0b8929ddf88fdb
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-12-04 16:45:10 -0800

    Beta/Protocol: Handle logins.

--- Baskerville/Beta/Protocol.hs
+++ Baskerville/Beta/Protocol.hs
@@ -1,12 +1,14 @@
 module Baskerville.Beta.Protocol where
 
 import Control.Monad
+import Control.Monad.ST
 import Control.Monad.Trans.Class
 import qualified Data.ByteString as BS
 import Data.IterIO
 import Data.IterIO.Atto
 import Data.List
 import Data.Serialize
+import Data.STRef
 import qualified Data.Text as T
 
 import Baskerville.Beta.Packets
@@ -65,6 +67,24 @@ invalidPred _ = True
 --   reply. This function should be provided with state so that it can
 --   process consecutive packets.
 processPacket :: ProtocolState -> Packet -> (ProtocolState, [Packet])
+
+-- | Login. Examine all of the bits, make sure they match, and then reply in
+--   kind.
+processPacket ps (LoginPacket protocol _ _ _ _ _ _ _) = runST $ do
+    state <- newSTRef ps
+    packets <- newSTRef []
+    -- Is the protocol invalid? Kick the client with an unsupported-protocol
+    -- message.
+    if (protocol /= 22)
+        then do
+            modifySTRef state (\x -> x { psStatus = Invalid })
+            modifySTRef packets (ErrorPacket (T.pack "Unsupported protocol") :)
+        else do
+            modifySTRef state (\x -> x { psStatus = Connected })
+            modifySTRef packets (LoginPacket 1 T.empty 0 Creative Earth 1 128 1 :)
+    newps <- readSTRef state
+    newpackets <- readSTRef packets
+    return (newps, newpackets)
 
 -- | Handshake. Just write down the username.
 processPacket ps (HandshakePacket nick) =
