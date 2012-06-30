47475b9bb2021566f03a6b2b9246754b58e1bf3e
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-06-30 12:27:18 -0700

    Main: Get a packet conduit up and running.
    Huh. That was easy. ...Too easy...

--- Baskerville/Main.hs
+++ Baskerville/Main.hs
@@ -1,15 +1,33 @@
 module Main where
 
 import Network
+import qualified Data.Bytestring as BS
 import Data.Conduit
+import Data.Conduit.Cereal
+import qualified Data.Conduit.List as CL
 import Data.Conduit.Network
+import Data.Serialize
 
--- import Baskerville.Beta.Protocol
+import Baskerville.Beta.Packets
+
+toPackets :: Conduit BS.ByteString IO Packet
+toPackets = conduitGet $ get
+
+fromPackets :: Conduit Packet IO BS.ByteString
+fromPackets = conduitPut $ put
+
+worker :: Conduit Packet IO Packet
+worker = CL.mapM $ \packet -> do
+    putStrLn "Got a packet!"
+    putStrLn $ show packet
+    return packet
 
 app :: Application IO
 app source sink = do
     putStrLn "Before app..."
-    source $$ sink
+    let pSource = source $= toPackets
+    let pSink = fromPackets =$ sink
+    pSource $$ worker =$ pSink
     putStrLn "After app!"
 
 startServer :: IO ()
