822986e6782b6e305de32adaff6dc845f5b079a8
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-07-01 11:15:54 -0700

    Coords: Add lenses for BCoord.

--- Baskerville/Coords.hs
+++ Baskerville/Coords.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Baskerville.Coords where
 
 import Data.Ix
+import Data.Lens.Template
 
-data BCoord = BCoord { bcx :: Int, bcz :: Int, bcy :: Int }
+data BCoord = BCoord { _bcx :: Int, _bcz :: Int, _bcy :: Int }
     deriving (Eq, Ix, Ord, Show)
+
+$( makeLens ''BCoord )
