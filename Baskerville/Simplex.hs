8a973fa2a8e0506a2edf14a8e14eb9ce3d0ba18b
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-07-01 15:17:46 -0700

    Simplex: Get closer.
    Agrfual, why are numbers so difficult? I thought this was a maths language!

--- Baskerville/Simplex.hs
+++ Baskerville/Simplex.hs
@@ -5,12 +5,14 @@ import Data.List
 import System.Random.Shuffle
 
 -- | The edges of a two-dimensional simplex field.
-edges2 :: [[Integer]]
+edges2 :: [[Int]]
 edges2 = sort $ nub $ concat [
     permutations [0, 1, 1], permutations [0, 1, -1], permutations [0, -1, -1]]
 
+edge2 :: Int -> [Int]
+edge2 i = edges2 !! (i `mod` length edges2)
+
 -- | Dot product for lists of numbers.
-dot :: Num a => [a] -> [a] -> a
 dot u v = sum $ zipWith (*) u v
 
 -- | The constant 'f' for two-dimensional fields.
@@ -28,8 +30,30 @@ size = 1024
 field :: (MonadRandom m) => m [Integer]
 field = shuffleM [0 .. (size - 1)]
 
--- | Another thing!
-t :: (Fractional a) => a -> a -> a
-t x y = 0.5 - (x^2) - (y^2)
+-- | Squish 2D coords onto a simplex grid.
+--   Returns a tuple of the ints for array lookups, and the fractional parts
+--   for inter-box interpolation. You could think of it as (I, J, X, Y).
+squish2 x y = let
+    s = (x + y) * f2
+    i = floor $ x + s
+    j = floor $ y + s
+    t = fromIntegral (i + j) * g2
+    in (i, j, x + t - fromIntegral i, y + t - fromIntegral j)
 
--- simplex2 :: Num a, Num b, Floating c => a -> b -> b -> c
+coords2 x y = if x > y
+    then [(x, y), (x - 1 + g2, y + g2), (x - 1 + g2 * 2, y - 1 + g2 * 2)]
+    else [(x, y), (x + g2, y - 1 + g2), (x - 1 + g2 * 2, y - 1 + g2 * 2)]
+
+-- | Interpolator for 2D coords.
+t2 x y = 0.5 - (x^2) - (y^2)
+
+n2 (x, y) g = let t = t2 x y in
+    if t > 0 then (t^4) * dot [x, y] (map fromIntegral $ edge2 g) else 0
+
+simplex2 :: (MonadRandom m) => Double -> Double -> m Double
+simplex2 sx sy = let
+    (i, j, dx, dy) = squish2 sx sy
+    coords = coords2 dx dy
+    gradients = []
+    n = sum $ zipWith n2 coords gradients
+    in undefined
