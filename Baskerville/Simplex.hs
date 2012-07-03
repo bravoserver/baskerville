913970a039807d0e5c05b7a6a17c35cacfb736ac
Justin Noah <justinnoah@gmail.com>
2012-07-02 22:38:38 -0700

    Silenced some warnings in Simplex.hs

--- Baskerville/Simplex.hs
+++ Baskerville/Simplex.hs
@@ -13,6 +13,7 @@ edge2 :: Int -> [Int]
 edge2 i = edges2 !! (i `mod` length edges2)
 
 -- | Dot product for lists of numbers.
+dot :: (Num a) => [a] -> [a] -> a
 dot u v = sum $ zipWith (*) u v
 
 -- | The constant 'f' for two-dimensional fields.
@@ -33,6 +34,7 @@ field = shuffleM [0 .. (size - 1)]
 -- | Squish 2D coords onto a simplex grid.
 --   Returns a tuple of the ints for array lookups, and the fractional parts
 --   for inter-box interpolation. You could think of it as (I, J, X, Y).
+squish2 :: (Integral a) => Double -> Double -> (a, a, Double, Double)
 squish2 x y = let
     s = (x + y) * f2
     i = floor $ x + s
@@ -40,13 +42,16 @@ squish2 x y = let
     t = fromIntegral (i + j) * g2
     in (i, j, x + t - fromIntegral i, y + t - fromIntegral j)
 
+coords2 :: Double -> Double -> [(Double, Double)]
 coords2 x y = if x > y
     then [(x, y), (x - 1 + g2, y + g2), (x - 1 + g2 * 2, y - 1 + g2 * 2)]
     else [(x, y), (x + g2, y - 1 + g2), (x - 1 + g2 * 2, y - 1 + g2 * 2)]
 
 -- | Interpolator for 2D coords.
+t2 :: (Fractional a) => a -> a -> a
 t2 x y = 0.5 - (x^2) - (y^2)
 
+n2 :: (Fractional a, Ord a) => (a, a) -> Int -> a
 n2 (x, y) g = let t = t2 x y in
     if t > 0 then (t^4) * dot [x, y] (map fromIntegral $ edge2 g) else 0
 
