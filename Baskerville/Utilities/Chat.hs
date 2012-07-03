ada5d76f0886910fae94af4cdd06cfeb2f6da9a2
Justin Noah <justinnoah@gmail.com>
2012-07-02 21:54:45 -0700

    Silenced warnings in Utilities\Chat.hs

--- Baskerville/Utilities/Chat.hs
+++ Baskerville/Utilities/Chat.hs
@@ -2,12 +2,19 @@ module Baskerville.Utilities.Chat where
 
 import Data.Char
 
+hashStringIndex :: [Char] -> Int
 hashStringIndex s = foldr ((+) . ord) 0 s `mod` 15
 
+betaColor :: Int -> [Char]
 betaColor i = ['§', "0123456789abcde" !! i]
+
+consoleColor :: Int -> [Char]
 consoleColor i =
     "\x1b[" ++ (["1;30", "34", "32", "36", "31", "35", "33", "1;37", "37",
     "1;34", "1;32", "1;36", "1;31", "1;35", "1;33"] !! i) ++ "m"
 
+betaChatName :: [Char] -> [Char]
 betaChatName s = betaColor (hashStringIndex s) ++ s ++ "§f"
+
+consoleChatName :: [Char] -> [Char]
 consoleChatName s = consoleColor (hashStringIndex s) ++ s ++ "\x1b[0m"
