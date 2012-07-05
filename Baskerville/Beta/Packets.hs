f7fab913b20e84d3e242b961661cde3f724f4b21
Justin Noah <justinnoah@gmail.com>
2012-07-04 23:50:18 -0700

    Packets!

--- Baskerville/Beta/Packets.hs
+++ Baskerville/Beta/Packets.hs
@@ -94,6 +94,71 @@ instance Serialize Item where
         q <- getWord8
         return $ Item p s q
 
+-- | Player animations
+data Animation = NoAnim | SwingArmAnim | DamageAnim | ExitBedAnim | EatAnim | CrouchAnim | UncrouchAnim
+    deriving (Eq, Show)
+
+instance Serialize Animation where
+    put NoAnim = putWord8 0x00
+    put SwingArmAnim = putWord8 0x01
+    put DamageAnim = putWord8 0x02
+    put ExitBedAnim = putWord8 0x03
+    put EatAnim = putWord8 0x05
+    put CrouchAnim = putWord8 0x68
+    put UncrouchAnim = putWord8 0x69
+    get = do
+        d <- getWord8
+        return $ case d of
+            0x01 -> SwingArmAnim
+            0x02 -> DamageAnim
+            0x03 -> ExitBedAnim
+            0x05 -> EatAnim
+            0x68 -> CrouchAnim
+            0x69 -> UncrouchAnim
+            _    -> NoAnim
+
+-- | Player Actions
+data Action = CrouchAct | UncrouchAct | ExitBedAct | SprintAct | UnSprintAct
+    deriving (Eq, Show)
+
+instance Serialize Action where
+    put CrouchAct = putWord8 0x01
+    put UncrouchAct = putWord8 0x02
+    put ExitBedAct = putWord8 0x03
+    put SprintAct = putWord8 0x04
+    put UnSprintAct = putWord8 0x05
+    get = do
+        d <- getWord8
+        return $ case d of
+            0x01 -> CrouchAct
+            0x02 -> UncrouchAct
+            0x03 -> ExitBedAct
+            0x04 -> SprintAct
+            0x05 -> UnSprintAct
+    -- | Should be made exhaustive, not sure which to default to.
+
+-- | Objects and Vehicles
+data ObVehicle = BoatOb | MinecartOb | MinecartStorageOb | MinecartPoweredOb | TNTOb | EnderCrystalOb | ArrowOb | SnowballOb | EggOb | SandOb | GravelOb | EyeOfEnderOb | DragonEggOb | FishingFloatOb
+    deriving (Eq, Show)
+
+-- | ObVehicle probably needs a rename, but more importantly...serialization
+
+data PaintDirection = PPosX | PNegX | PPosZ | PNegZ
+    deriving (Eq, Show)
+
+instance Serialize PaintDirection where
+    put PPosX = putWord32be 0x03
+    put PNegX = putWord32be 0x01
+    put PPosZ = putWord32be 0x02
+    put PNegZ = putWord32be 0x00
+    get = do
+        d <- getWord32be
+        return $ case d of
+            0x03 -> PPosX
+            0x02 -> PPosZ
+            0x01 -> PNegX
+            _    -> PNegZ
+
 -- | Pack a text string into a UCS2 length-prefixed string.
 bUcs2 :: T.Text -> BS.ByteString
 bUcs2 t = BS.append (bWord16 $ fromIntegral (T.length t)) (encodeUtf16BE t)
@@ -117,18 +182,74 @@ getUcs2 = do
 --   servers. They are atomic and self-contained. The first byte of a packet
 --   identifies the packet, and the payload is immediately inlined, with no
 --   delimiters. This makes packets difficult to parse.
-data Packet = PingPacket Word32
+data Packet = PingPacket Word32 -- 0x00
             | LoginPacket Word32 T.Text T.Text Mode Dimension Difficulty Word8 Word8
-            | HandshakePacket T.Text
-            | ChatPacket T.Text
-            | TimePacket Word64
-            | EquipmentPacket Word32 Word16 Word16 Word16
-            | SpawnPacket Word32 Word32 Word32
-            | UsePacket Word32 Word32 Word8
-            | RespawnPacket Dimension Difficulty Mode Word16 T.Text
-            | AirbornePacket Airborne
-            | DiggingPacket DiggingState Word32 Word8 Word32
-            | BuildPacket Word32 Word8 Word32 Face Item
+            | HandshakePacket T.Text -- 0x02
+            | ChatPacket T.Text -- 0x03
+            | TimePacket Word64 -- 0x04
+            | EquipmentPacket Word32 Word16 Word16 Word16 -- 0x05
+            | SpawnPacket Word32 Word32 Word32 -- 0x06
+            | UsePacket Word32 Word32 Bool -- 0x07
+            | UpdateHealth Word16 Word16 Float -- 0x08
+            | RespawnPacket Dimension Difficulty Mode Word16 T.Text -- 0x09
+            | AirbornePacket Airborne -- 0x0A | Known as Player on kev009
+            | PositionPacket Double Double Double Double Airborne -- 0x0B
+            | LookPacket Float Float Airborne -- 0x0C
+            | PositionAndLookPacket Double Double Double Double Float Float Airborne -- 0x0D
+            | DiggingPacket DiggingState Word32 Word8 Word32 Face -- 0x0E
+            -- | PlaceBlockPacket WorldDirection Word8 Word32 Word8 Slot -- 0x0F
+            | SlotSelectionPacket Word16 -- 0x10
+            | UseBedPacket Word32 Word8 Word32 Word8 Word32 -- 0x11
+            | AnimationPacket Word32 Animation -- 0x12
+            | ActionPacket Word32 Action -- 0x13
+            | SpawnNamedPacket Word32 T.Text Word32 Word32 Word32 Word8 Word8 Item -- 0x14
+            | SpawnDroppedPacket Word32 Item Word8 Word16 Word32 Word32 Word32 Word8 Word8 Word8 -- 0x15
+            | CollectItemPacket Word32 Word32 -- 0x16
+            -- Two 0x17 possibilities
+            -- | SpawnObjectPacket Word32 ObVehicle Word32 Word32 Word32 Word32 -- 0x17
+            -- | SpawnObjectPacket Word32 ObVehicle Word32 Word32 Word32 Word32 Word16 Word16 Word16 -- 0x17
+            -- | SpawnMobPacket Word32 Mob Word32 Word32 Word32 Word8 Word8 Word8 Metadata -- 0x18
+            | SpawnPaintingPacket Word32 T.Text Word32 Word32 Word32 PaintDirection -- 0x19
+            | SpawnExperiencePacket Word32 Word32 Word32 Word32 Word16 -- 0x1A
+            | VelocityPacket Word32 Word16 Word16 Word16 -- 0x1C
+            | DestroyEntityPacket Word32 -- 0x1D
+            | EntityPacket Word32 -- 0x1E
+            | RelativeMovePacket Word32 Word8 Word8 Word8 -- 0x1F
+            | EntityLookPacket Word32 Word8 Word8 -- 0x20
+            | EntityLookMovePacket Word32 Word8 Word8 Word8 Word8 Word8 -- 0x21
+            | EntityTeleportPacket Word32 Word32 Word32 Word32 Word8 Word8 -- 0x22
+            | EntityHeadLookPacket Word32 Word8 -- 0x23
+            -- | EntityStatus Word32 Status -- 0x26
+            | EntityAttachPacket Word32 Word32 -- 0x27
+            -- | EntityMetadataPacket Word32 Metadata -- 0x28
+            | EntityEffectPacket Word32 Word8 Word8 Word16 -- 0x29
+            | EndEntityEffectPacket Word32 Word8 -- 0x2A
+            | SetExperiencePacket Float Word16 Word16 -- 0x2B
+            | AllocChunkPacket Word32 Word32 Bool -- 0x32
+            -- | ChunkPacket Word32 Word32 Word8 Word16 Word16 Word32 Word32 ChunkData -- 0x33
+            -- | MultiBlockChangePacket Word32 Word32 Word16 Word32 MultiBlockData -- 0x34
+            | BlockChangePacket Word32 Word8 Word32 Word8 Word8 -- 0x35
+            -- | BlockActionPacket Word32 Word16 Word32 BlockAction BlockAction -- 0x36
+            -- | ExplosionPacket Double Double Double Float Word32 [Record] -- 0x3C
+            -- | SoundParticleEffectPacket Word32 Word32 Word8 Word32 EffectData -- 0x3D
+            -- | GameStateChangePacket Reason Difficulty -- 0x46
+            | ThunderboltPacket Word32 Word8 Word32 Word32 Word32 -- 0x47
+            | WindowOpenPacket Word8 Word8 T.Text Word8 -- 0x64
+            | WindowClosePacket Word8 -- 0x65
+            -- | WindowClickPacket Word8 Word16 Word8 Word16 Bool Slot -- 0x66
+            -- | WindowSlotChangePacket Word8 Word16 Slot -- 0x67
+            -- | SetWindowItemsPacket Word8 Word16 [Slot] -- 0x68
+            -- | UpdateWindowPacket Word8 WindowProperty Word16 -- 0x69
+            -- | ConfirmationPacket Word8 Word16 Accepted -- 0x6A
+            -- | CreativeInventoryActionPacket Word16 Slot -- 0x6B
+            | EnchantItemPacket Word8 Word8 -- 0x6C
+            | UpdateSignPacket Word32 Word16 Word32 T.Text T.Text T.Text T.Text -- 0c82
+            -- | ItemDataPacket Item Word16 Word8 [Word8] -- 0x83
+            -- | UpdateOnTileEntity Word32 Word16 Word32 UpdateType Word32 Word32 Word32 -- 0x84
+            -- | AlterStatPacket Stat Word8 -- 0xC8
+            | PlayerListPacket T.Text Bool Word16 -- 0xC9
+            -- | PlayerAbilities Invulnerable Flying Flyable InstaBreak -- 0xCA
+            -- | PluginMessagePacket T.Text Word16 [Word8] -- 0xFA
             | PollPacket
             | ErrorPacket T.Text
             | InvalidPacket
