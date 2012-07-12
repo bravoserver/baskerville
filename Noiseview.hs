1d934674553ce7cc0bea0dcbfbe6812d7caab11f
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-07-12 06:38:51 -0700

    Noiseview: Do 800x800, like Bravo.

--- Noiseview.hs
+++ Noiseview.hs
@@ -6,16 +6,18 @@ import System.Random
 
 readDouble x = read x :: Double
 
+dim = 800
+
 main = do
     ssx : ssy : _ <- getArgs
     let sx = readDouble ssx
     let sy = readDouble ssy
     putStrLn "P2"
-    putStrLn "512 512"
+    putStrLn $ show dim ++ " " ++ show dim
     putStrLn "255"
-    forM [0..511] $ \x -> do
-        forM [0..511] $ \y -> do
-            let pix = octaves2 5 0 (x * sx / 512) (y * sy / 512)
+    forM [0..(dim - 1)] $ \x -> do
+        forM [0..(dim - 1)] $ \y -> do
+            let pix = octaves2 5 0 (x * sx / dim) (y * sy / dim)
             let adjusted = floor $ (pix + 1) * 127.5
             putStr $ show adjusted ++ " "
         putStrLn " "
