32bfac57f4ea7c05b662b331936c9c73fd3e6ba4
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-07-11 22:57:46 -0700

    Fix malformed PNM.

--- Noiseview.hs
+++ Noiseview.hs
@@ -7,6 +7,7 @@ import System.Random
 main = do
     putStrLn "P2"
     putStrLn "512 512"
+    putStrLn "255"
     forM [0..511] $ \x -> do
         forM [0..511] $ \y -> do
             let pix = simplex2 0 (x/512) (y/512)
