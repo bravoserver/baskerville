632751453d1e4bcc7a49bf8411dec0fa3c2a5ada
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-07-12 06:08:05 -0700

    Noiseview: Add more options.

--- Noiseview.hs
+++ Noiseview.hs
@@ -4,13 +4,18 @@ import Control.Monad.Random
 import System.Environment
 import System.Random
 
+readDouble x = read x :: Double
+
 main = do
+    ssx : ssy : _ <- getArgs
+    let sx = readDouble ssx
+    let sy = readDouble ssy
     putStrLn "P2"
     putStrLn "512 512"
     putStrLn "255"
     forM [0..511] $ \x -> do
         forM [0..511] $ \y -> do
-            let pix = simplex2 0 (x/512) (y/512)
+            let pix = simplex2 0 (x * sx / 512) (y * sy / 512)
             let adjusted = floor $ (pix + 1) * 127.5
             putStr $ show adjusted ++ " "
         putStrLn " "
