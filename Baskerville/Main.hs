ac052c20fe798d2a10d75137e45dccedd7fcb095
Corbin Simpson <cds@corbinsimpson.com>
2013-10-17 19:53:58 -0700

    Main: Redo entire system architecture to be simpler.
    Instead of weird-ass StateT hackery, we're gonna just use STM.

--- Baskerville/Main.hs
+++ Baskerville/Main.hs
@@ -2,61 +2,72 @@
 
 module Main where
 
-import Control.Distributed.Process
-import Control.Distributed.Process.Node
-import Control.Exception
-import Control.Monad
-import Control.Monad.Trans.State (evalStateT)
-import Data.Binary
+import Control.Concurrent hiding (yield)
+import Control.Concurrent.STM
+import Control.Monad.IO.Class
 import Data.Conduit
 import Data.Conduit.Network
-import Data.Typeable
 import Network
-import Network.Transport.TCP
 
 import Baskerville.Beta.Conduits
+import Baskerville.Beta.Packets
 import Baskerville.Beta.Session
 
-app :: Application (Session IO)
-app appdata = do
-    liftIO $ putStrLn "Before app..."
+intake :: TChan (Maybe Packet) -> Sink Packet IO ()
+intake chan = loop
+    where
+    loop = do
+        mpacket <- await
+        case mpacket of
+            Nothing -> liftIO $ putStrLn "Finished reading!"
+            Just _  -> do
+                liftIO . atomically $ writeTChan chan mpacket
+                loop
+
+outflow :: TChan (Maybe Packet) -> Source IO Packet
+outflow chan = loop
+    where
+    loop = do
+        mpacket <- liftIO . atomically $ readTChan chan
+        case mpacket of
+            Nothing     -> liftIO $ putStrLn "Finished writing!"
+            Just packet -> do
+                yield packet
+                loop
+
+makeChans :: () -> STM (TChan (Maybe Packet), TChan (Maybe Packet))
+makeChans core = do
+    incoming <- newTChan
+    outgoing <- newTChan
+    return (incoming, outgoing)
+
+packetThread :: TChan (Maybe Packet) -> TChan (Maybe Packet) -> IO ()
+packetThread incoming outgoing = loop
+    where
+    loop = do
+        putStrLn "Start"
+        atomically $ do
+            p <- readTChan incoming
+            writeTChan outgoing p
+        putStrLn "End"
+        loop
+
+app :: TVar () -> Application IO
+app tcore appdata = do
+    putStrLn "Before app..."
     let pSource = appSource appdata $= bsToPackets
         pSink = packetsToBs =$ appSink appdata
-    pSource $$ protocol =$ pSink
-    liftIO $ putStrLn "After app!"
+    (incoming, outgoing) <- atomically $ makeChans ()
+    liftIO . forkIO $ pSource $$ intake incoming
+    liftIO . forkIO $ outflow outgoing $$ pSink
+    packetThread incoming outgoing
+    putStrLn "After app!"
 
-startServer :: (Session IO) ()
-startServer = runTCPServer (serverSettings 25565 HostAny) app
-
-data NewSession = NewSession
-    deriving (Typeable, Show)
-
-instance Binary NewSession where
-    put NewSession = putWord8 0
-    get = getWord8 >> return NewSession
-
-slProcess :: ReceivePort NewSession -> Process ()
-slProcess rp = do
-    message <- receiveChan rp
-    case message of
-        NewSession -> say "I was asked to create a new session!"
-    slProcess rp
-
-startSessionList :: Process (SendPort NewSession)
-startSessionList = spawnChannelLocal slProcess
+startServer :: TVar () -> IO ()
+startServer tcore = runTCPServer (serverSettings 25565 HostAny) $ app tcore
 
 main :: IO ()
 main = withSocketsDo $ do
-    putStrLn "Starting TCP control socket..."
-    etrans <- createTransport "localhost" "omniorb" defaultTCPParameters
-    transport <- case etrans of
-        Left exception -> throw exception
-        Right t -> return t
-    putStrLn "Creating local node..."
-    node <- newLocalNode transport initRemoteTable
-    putStrLn "Spinning up SessionList..."
-    pid <- forkProcess node (void startSessionList)
-    putStrLn $ "Forked, PID " ++ show pid
     putStrLn "Starting up..."
-    s <- startingState
-    evalStateT startServer s
+    core <- atomically $ newTVar ()
+    startServer core
